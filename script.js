bf2244ad2936f4166cf5fc0376539b6bb3cbff8a
Adam_442 <aam_442@hotmail.com>
2022-09-28 23:02:41 +0300

    Updated Night Mode and mobile layout.

--- script.js
+++ script.js
@@ -1,36 +1,24 @@
-let light = {
-    '--accent': '#357657',
-    '--text': 'black',
-    '--text-hover': 'black',
-    '--background': '#f8f8f8',
-    '--card': '#f8f8f8',
-    '--footer': '#357657',
-    '--btn': 'black',
-    '--btn-hover': '#f8f8f8',
-}
-let dark = {
-    '--accent': '#357657',
-    '--text': '#f8f8f8',
-    '--text-hover': '#f8f8f8',
-    '--background': '#424242',
-    '--card': '#212121',
-    '--footer': '#303030',
-    '--btn': '#424242',
-    '--btn-hover': '#357657',
-}
-
-let themes = [light, dark];
+const PICS = ['images/me.png', 'images/me2.png'];
+const ROOT = document.documentElement;
+let themes;
 let t = 1;
 
-let root = document.documentElement;
-for (const [key, value] of Object.entries(themes[t])) {
-    root.style.setProperty(key, value);
-}
+async function theme() {
+    if (!themes) {
+        await fetch('themes.json')
+            .then((response) => response.json())
+            .then((json) => themes = [json["light"], json["dark"]]);
+    }
 
-function theme() {    
     t = (t)? 0: 1;
 
     for (const [key, value] of Object.entries(themes[t])) {
-        root.style.setProperty(key, value);
+        ROOT.style.setProperty(key, value);
     }
+
+    // change profile picture: 1. hide it, 2. change picture after 300ms (transition time), 3. show it again (transition will play again!).
+    let pfp = document.getElementById('pfp');
+    pfp.style.opacity = '0%';
+    setTimeout(() => pfp.src = PICS[t], 301);
+    setTimeout(() => pfp.style.opacity = '100%', 311);
 }
